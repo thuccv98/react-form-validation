e62e8acaeddee0d8be5a2aa96c8ee228aaacc869
thuccv98 <thuccv98@gmail.com>
2021-08-12 17:45:21 +0700

    Completed handle form validation

--- src/App.js
+++ src/App.js
@@ -1,28 +1,73 @@
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
 import './App.css';
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const [userInfo, setUserInfo] = useState();
+
+  const onSubmit = (data) => {
+    setUserInfo(data);
+  };
+
   return (
     <div className="container">
-      <form>
+      <pre>{JSON.stringify(userInfo, undefined, 2)}</pre>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <h1>Registration Form</h1>
         <div className="ui divider"></div>
         <div className="ui form">
           <div className="field">
             <label>Username</label>
-            <input type="text" name="username" placeholder="Enter Username" />
+            <input
+              type="text"
+              name="username"
+              placeholder="Enter Username"
+              {...register('username', { required: 'Username is required' })}
+            />
           </div>
+          <p>{errors.username?.message}</p>
           <div className="field">
             <label>Email</label>
-            <input type="email" name="email" placeholder="Enter Email" />
+            <input
+              type="email"
+              name="email"
+              placeholder="Enter Email"
+              {...register('email', {
+                required: 'Email is required',
+                pattern: {
+                  value: /^\S+@\S+$/i,
+                  message: 'This is not a valid email',
+                },
+              })}
+            />
           </div>
+          <p>{errors.email?.message}</p>
           <div className="field">
             <label>Password</label>
             <input
               type="password"
               name="password"
               placeholder="Enter Password"
+              {...register('password', {
+                required: 'Password is required',
+                minLength: {
+                  value: 6,
+                  message: 'Password must be more than 6 characters',
+                },
+                maxLength: {
+                  value: 15,
+                  message: 'Password cannot exceed more than 15 characters',
+                },
+              })}
             />
           </div>
+          <p>{errors.password?.message}</p>
           <button className="fluid ui button blue">Submit</button>
         </div>
       </form>
